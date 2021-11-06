67afe9a4641a9ef39b11c0dd7c1bdfd14f27101b
Debuger <debugersoftware@outlook.com>
2021-11-06 07:01:00 +0100

    Delete credentials.h

--- credentials.h
+++
@@ -1,6 +0,0 @@
-
-// const char*   ssid          =       "REZYSERKA";
-// const char*   password      =       "#Gopl@n@2#";
-
-const char*   ssid          =       "_db_tmp";
-const char*   password      =       "89052311539";
